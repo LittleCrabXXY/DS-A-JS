4787859655d7a0852ef586a09d647f9cdff2a816
LittleCrabXXY <LittleCrabXXY@163.com>
2017-02-24 16:45:13 +0800

    add exercise2-2

---
+++ Ch2-Array/exercise2-2.js
@@ -0,0 +1,7 @@
+/**
+ * 将一组单词存储在一个数组中，并按正序和倒序分别显示这些单词。
+ */
+
+var words = ['browser' ,'weather', 'wind', 'house', 'classroom', 'translate'];
+console.log(words.sort().toString());
+console.log(words.sort().reverse().toString());
